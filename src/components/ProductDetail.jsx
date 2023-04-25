9d34b089eb7e5191d74f6b43a9d9218ba8fd9be2
manojkumar021997 <manojkumar020m@gmail.com>
2023-04-25 16:33:07 +0530

    changed images in src

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -8,7 +8,7 @@ import {addItem, delItem} from '../redux/actions/index'
 const ProductDetail = () => {
     const [cartBtn, setCartBtn] = useState("Add to Cart")
     const proid = useParams();
-    const proDetail = DATA.filter(x=>x.id == proid.id)
+    const proDetail = DATA.filter(x=>x.id === proid.id)
     const product = proDetail[0];
     console.log(product);
 
